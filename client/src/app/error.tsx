3450346930e14faa640878770183749d18eb8e40
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-11-15 21:38:39 +0530

    feat: added date time meetings

---
+++ client/src/app/error.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+const ErrorPage = () => {
+	return (
+		<div className="space-y-4 text-center">
+			<h1 className="text-2xl font-bold">Error</h1>
+			<p>Sorry, Something went wrong. Please try later.</p>
+		</div>
+	)
+}
+
+export default ErrorPage
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
+import { CreateMeeting } from "./CreateMeeting";
+
 export default function Home() {
 	return (
-		<div>
-			<h1>Home</h1>
-		</div>
+		<CreateMeeting/>
 	)
 }
