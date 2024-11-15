475e0343822da1fa5eff7ea9f57b322c345e16cb
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-11-15 23:28:08 +0530

    feat: added create meetings

--- client/src/app/CreateMeeting.tsx
+++ client/src/app/CreateMeeting.tsx
@@ -1,14 +1,34 @@
 'use client'
 import { useUser } from '@clerk/nextjs'
 import { Loader2 } from 'lucide-react'
-import { useStreamVideoClient } from '@stream-io/video-react-sdk'
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
 import { useState } from 'react'
 
 export const CreateMeeting = () => {
 	const [descriptionInput, setDescriptionInput] = useState('')
 	const [startTimeInput, setStartTimeInput] = useState('')
+	const [participantInput, setParticipantInput] = useState('')
+	const [call, setCall] = useState<Call | null>(null)
 	const client = useStreamVideoClient()
 	const { user } = useUser()
+	const createMeeting = async () => {
+		if (!client || !user) return
+		try {
+			const id = crypto.randomUUID()
+			const call = client.call('default', id)
+			await call.getOrCreate({
+				data: {
+					custom: {
+						description: descriptionInput
+					}
+				}
+			})
+			setCall(call)
+		} catch (error) {
+			console.error(error)
+			alert('Something went wrong.Please try again.')
+		}
+	}
 	if (!client || !user) {
 		return <Loader2 className="mx-auto animate-spin" />
 	}
@@ -19,6 +39,10 @@ export const CreateMeeting = () => {
 				<h2 className="text-xl font-medium">Create a new Meeting</h2>
 				<DescriptionInput value={descriptionInput} onChange={setDescriptionInput} />
 				<StartTimeInput value={startTimeInput} onChange={setStartTimeInput} />
+				<ParticipantInput value={participantInput} onChange={setParticipantInput} />
+				<button onClick={createMeeting} className="w-full rounded-md bg-blue-500 p-2 text-white">
+					Create Meeting
+				</button>
 			</div>
 		</div>
 	)
@@ -101,6 +125,52 @@ const StartTimeInput = ({ value, onChange }: StartTimeInputProps) => {
 						value={value}
 						onChange={(e) => onChange(e.target.value)}
 						min={dateTimeLocalNow}
+						className="w-full rounded-md border border-slate-300 p-2"
+					/>
+				</label>
+			)}
+		</div>
+	)
+}
+
+interface ParticipantInput {
+	value: string
+	onChange: (value: string) => void
+}
+
+const ParticipantInput = ({ value, onChange }: ParticipantInput) => {
+	const [active, setActive] = useState(false)
+	return (
+		<div className="space-y-2">
+			<div className="font-medium">Participant:</div>
+			<label className="flex items-center gap-1.5">
+				<input
+					type="radio"
+					checked={!active}
+					onChange={() => {
+						setActive(false)
+						onChange('')
+					}}
+				/>
+				Everyone with link can join
+			</label>
+			<label className="flex items-center gap-1.5">
+				<input
+					type="radio"
+					checked={active}
+					onChange={() => {
+						setActive(true)
+					}}
+				/>
+				Private Meeting
+			</label>
+			{active && (
+				<label className="block space-y-1">
+					<span className="font-medium">Participant Email</span>
+					<textarea
+						value={value}
+						onChange={(e) => onChange(e.target.value)}
+						placeholder="Enter email addresses separated by commas"
 						className="w-full rounded-md border border-slate-300 p-2"
 					/>
 				</label>
