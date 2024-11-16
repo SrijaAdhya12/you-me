37eb4cafe6478e2ebc60b0d02caa86e881485cac
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-11-16 18:31:04 +0530

    feat: added stream video interface

--- client/src/app/meeting/[id]/page.tsx
+++ client/src/app/meeting/[id]/page.tsx
@@ -1,22 +1,19 @@
 import { Metadata } from "next"
 import MeetingPage from "./MeetingPage"
 
-interface PageParams {
-	id: string
-}
+
 
 interface PageProps {
 	params: {
 		id: string
-		params: PageParams
 	}
 }
 
-export const generateMetadata = async ({ params: { id } }: PageProps): Promise<Metadata> => {
-	return {
-		title: `Meeting ${id}`
-	}
-}
+// export const generateMetadata = async ({ params: { id } }: PageProps): Promise<Metadata> => {
+// 	return {
+// 		title: `Meeting ${id}`
+// 	}
+// }
 
 const Page = ({ params: { id } }: PageProps) => {
 	return <MeetingPage id={id} />  
