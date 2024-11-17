9b5a95e7295fb2bebdfc603b3c56d891439714c0
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-11-17 14:54:40 +0530

    feat: added twmmerge

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,9 @@
+
+const Button = ({className, ...props}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+    return (
+        <button
+        className=""    {...props} />
+  )
+}
+
+export default Button
