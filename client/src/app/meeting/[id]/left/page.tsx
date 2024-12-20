b79b8afb7c1e18658d6a4c36dc4531e9950d1d75
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-12-20 11:35:33 +0530

    feat: fix deployment

--- client/src/app/meeting/[id]/left/page.tsx
+++ client/src/app/meeting/[id]/left/page.tsx
@@ -1,20 +1,23 @@
-import { buttonClassName } from "@/components/Button"
-import { cn } from "@/libs/utils"
-import Link from "next/link"
+import { buttonClassName } from '@/components/Button'
+import { cn } from '@/libs/utils'
+import Link from 'next/link'
 
-interface PageProps {
-	params: {
-		id: string
+type PageProps = {
+	params: Promise<{ id: string }> & { id: string }
+	searchParams: Promise<{ [key: string]: string | string[] | undefined }> & {
+		[key: string]: string | string[] | undefined
 	}
 }
 
-const Page = ({ params: {id} }: PageProps) => {
-    return <div className="flex flex-col items-center gap-3">
-        <p className="font-bold">
-            You left the meeting
-        </p>
-        <Link href={`/meeting/${id}`} className={cn(buttonClassName, "bg-gray-500 hover:bg-gray-600")}>Rejoin</Link>
-    </div>
+export default async function Page(props: PageProps) {
+	const { id } = await props.params 
+	const searchParams = await props.searchParams 
+	return (
+		<div className="flex flex-col items-center gap-3">
+			<p className="font-bold">You left the meeting</p>
+			<Link href={`/meeting/${id}`} className={cn(buttonClassName, 'bg-gray-500 hover:bg-gray-600')}>
+				Rejoin
+			</Link>
+		</div>
+	)
 }
-
-export default Page
